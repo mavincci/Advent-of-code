92029894f1115ae5e9c3026c0731aceb74aad531
mavincci <notthismichael@gmail.com>
2020-03-17 14:05:13 +0300

    Both Day 1 challenges completed

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -2,11 +2,15 @@
 #include <cmath>
 
 int main() {
-  int mass{};
+  int mass{}, temp{};
   long long total{};
 
   while(std::cin >> mass)
-    total += std::round(mass / 3) - 2;
+    for(;; mass=temp) {
+      temp = std::round(mass / 3) - 2;
+      if(temp < 0) break;
+      total += temp;
+    }
 
   std::cout << total;
   
